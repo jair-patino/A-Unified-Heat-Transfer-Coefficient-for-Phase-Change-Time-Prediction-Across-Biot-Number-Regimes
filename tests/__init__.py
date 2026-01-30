bce6b18e22afdbc617665ea0ac593bafe79c0297
jair-patino <jairpalejandrov@gmail.com>
2026-01-29 20:21:26 -0500

    Create __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+"""
+Test suite for the unified heat transfer model.
+
+This package contains unit tests for both the analytical model
+predictions and the numerical enthalpy solver.
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Jair Patiño B.'
